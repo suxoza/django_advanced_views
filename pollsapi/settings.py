fd03ad46603edb684dda0b8c79df43ce8c134002
Mikheil <suxoza@gmail.com>
2023-05-07 14:47:51 +0400

    swagger and docs

--- pollsapi/settings.py
+++ pollsapi/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     # external
     "rest_framework",
     "rest_framework.authtoken",
+    "rest_framework_swagger",
     # self
     "polls",
 ]
@@ -132,5 +133,6 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
-    )
+    ),
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
 }
--- pollsapi/urls.py
+++ pollsapi/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path, include
+from rest_framework_swagger.views import get_swagger_view
+from rest_framework.documentation import include_docs_urls
+
+schema_view = get_swagger_view(title="Polls API")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    #
+    # external
+    path(r"swagger-docs/", schema_view),
+    path(r"docs/", include_docs_urls(title="Polls API")),
+    # self
     re_path(r"^", include("polls.urls")),
 ]
